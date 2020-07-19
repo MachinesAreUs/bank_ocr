9d0fdaa095722e64b919efc6177014cba27ab625
Agustín Ramos <zentimental.software@gmail.com>
2020-07-19 13:08:12 -0500

    added matrix transpose tests

--- lib/util/matrix.ex
+++ lib/util/matrix.ex
@@ -1,7 +1,6 @@
 defmodule Util.Matrix do
   def transpose([]), do: []
   def transpose([[] | _]), do: []
-
   def transpose(a) do
     [Enum.map(a, &hd/1) | transpose(Enum.map(a, &tl/1))]
   end
---
+++ test/util/matrix_test.exs
@@ -0,0 +1,41 @@
+defmodule Util.MatrixTest do
+  use ExUnit.Case
+  alias Util.Matrix
+
+  test "transposes 3x3 matrix" do
+    orig = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
+    ]
+
+    expected = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ]
+
+    assert Matrix.transpose(orig) == expected
+  end
+
+  test "transposes 5x5 matrix" do
+    orig = [
+      [1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0],
+      [1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1]
+    ]
+
+    expected = [
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 1],
+      [0, 0, 1, 1, 1],
+      [0, 0, 0, 1, 1],
+      [0, 0, 0, 0, 1]
+    ]
+
+    assert Matrix.transpose(orig) == expected
+  end
+end
+
